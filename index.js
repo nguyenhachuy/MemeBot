f0281505f92b40bd6f1f232e07ae98bf8b704c15
nguyenhachuy <nguyenhachuy@gmail.com>
2017-07-03 02:58:21 -0700

    Adding some random stuff

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.login("xZzu9ZlBL-X0PhsoOXGSfxJJBR4OFao1");
-console.log("lmfao");
+client.login("MzMwOTY3MzMzNjkyNjM3MTg0.DDt37w.ANCTQs4cAq-HJ2JwBJyLXzYtaWQ");
+
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
+	let prefix = "!";
+	  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+ 	if(!message.guild) return;
+ 	if(!message.content.startsWith(prefix)) return;
+ 	if(message.content.startsWith(prefix +"ping")) {
+    	message.channel.send("pong!");
+  	} else
+
+  	if(message.content.startsWith(prefix + "foo")) {
+    	message.channel.send("bar!");
+  	}	
 });
+
+ 
+
